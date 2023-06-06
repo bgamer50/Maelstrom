370e542db77485eaf498fd532c2ce4421b6798b8
Alex Barghi <bgamer50@gmail.com>
2023-06-06 21:38:31 +0000

    minor update reduce by key

--- algorithms/impl/thrust/reduce_by_key.cu
+++ algorithms/impl/thrust/reduce_by_key.cu
@@ -2,9 +2,28 @@
 #include "algorithms/sort.h"
 #include "algorithms/select.h"
 
+template <typename E, typename K, typename V>
+std::pair<maelstrom::vector, maelstrom::vector> t_reduce_by_key_from_sorted_vec(E exec_policy, maelstrom::vector& input_keys, maelstrom::vector& input_values, maelstrom::reductor red, size_t max_unique_keys) {
+    
+}
+
+template <typename E, typename K>
+std::pair<maelstrom::vector, maelstrom::vector> reduce_by_key_from_sorted_vec_dispatch_val(E exec_policy, maelstrom::vector& input_keys, maelstrom::vector& input_values, maelstrom::reductor red, size_t max_unique_keys) {
+    
+}
+
+template <typename E>
+std::pair<maelstrom::vector, maelstrom::vector> reduce_by_key_from_sorted_vec_dispatch_key(E exec_policy, maelstrom::vector& input_keys, maelstrom::vector& input_values, maelstrom::reductor red, size_t max_unique_keys) {
+    
+}
+
+std::pair<maelstrom::vector, maelstrom::vector> reduce_by_key_from_sorted_vec_dispatch_exec_policy(maelstrom::vector& input_keys, maelstrom::vector& input_values, maelstrom::reductor red, size_t max_unique_keys) {
+
+}
+
 std::pair<maelstrom::vector, maelstrom::vector> reduce_by_key(maelstrom::vector& input_keys, maelstrom::vector& input_values, maelstrom::reductor red, size_t max_unique_keys, bool sorted=false) {
     if(sorted) {
-        return reduce_by_key_dispatch_exec_policy(input_keys, input_values, red, max_unique_keys);
+        return reduce_by_key_from_sorted_vec_dispatch_exec_policy(input_keys, input_values, red, max_unique_keys);
     }
 
     maelstrom::vector input_keys_copy(input_keys);
@@ -15,7 +34,7 @@ std::pair<maelstrom::vector, maelstrom::vector> reduce_by_key(maelstrom::vector&
     maelstrom::vector result_keys;
     maelstrom::vector result_vals;
 
-    std::tie(result_keys, result_vals) = reduce_by_key_dispatch_exec_policy(
+    std::tie(result_keys, result_vals) = reduce_by_key_from_sorted_vec_dispatch_exec_policy(
         input_keys_copy,
         input_values_copy,
         red,
